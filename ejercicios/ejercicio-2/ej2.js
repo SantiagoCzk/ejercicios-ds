67d13fc141b0b9b49384fe3f157bafa648029bca
santiago.czk <santiagoczk04@gmail.com>
2026-08-23 10:17:08 -0300

    Ejercicio 2

---
+++ ejercicios/ejercicio-2/ej2.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+    Crear una función que reciba dos valores y retorne su
+    multiplicación. Mostrar el resultado por consola.
+*/
+function multplicacion(a, b) {
+    return a * b;
+}
+let numero1 = 2;
+let numero2 = 5;
+console.log(`Resultado de multiplicar ${numero1} y ${numero2}: ${multplicacion(numero1, numero2)}`);
---
+++ ejercicios/ejercicio-2/ej2.ts
@@ -0,0 +1,14 @@
+/*
+    Crear una función que reciba dos valores y retorne su 
+    multiplicación. Mostrar el resultado por consola.  
+*/
+
+
+function multplicacion(a: number, b: number): number {
+    return a * b;
+}
+
+let numero1 = 2
+let numero2 = 5
+
+console.log(`Resultado de multiplicar ${numero1} y ${numero2}: ${multplicacion(numero1, numero2)}`)
