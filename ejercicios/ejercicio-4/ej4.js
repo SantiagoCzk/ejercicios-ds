8254d9a889e63b37cd41977bff601b05d4254b58
santiago.czk <santiagoczk04@gmail.com>
2026-08-23 10:51:04 -0300

    Ejercicio 4

---
+++ ejercicios/ejercicio-4/ej4.js
@@ -0,0 +1,7 @@
+/*
+    Crear un módulo con una función que sume dos números.
+    ● Importar y usar en archivo principal.
+    ● Mostrar resultados por consola en el archivo principal.
+*/
+import { suma } from "./suma.js";
+console.log(suma(4, 6));
---
+++ ejercicios/ejercicio-4/suma.ts
@@ -0,0 +1,6 @@
+
+export function suma(a: number, b: number): number
+{
+    return a + b;
+}
+
